ea422ae81554f3417e0585a39b7a381bc0462138
derick321 <derick321@kaist.ac.kr>
2024-07-22 18:04:20 +0900

    Fix start with google search

--- src/link.ts
+++ src/link.ts
@@ -1,24 +1,24 @@
-document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', async (event) => {
-        let from: string = window.location.href
-        let to: string = (<HTMLAnchorElement>event.target!).href
-        let tos: Array<string> = []
-        await chrome.storage.local.get(from).then(result => {
-            if (result[from] !== undefined) {
-                console.log("before: ", result[from])
-                tos = result[from]
-            }
-        })
-        if (tos.length > 0 && tos.indexOf(to) >= 0) {
-            console.log("no new visit")
-            return
-        }
-        tos.push(to)
-        let fromTo: any = {}
-        fromTo[from] = tos
-        await chrome.storage.local.set(fromTo)
-        chrome.storage.local.get(from, (result) => {
-            console.log("after: ", result)
-        })
-    })
-})
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", async (event) => {
+    let from: string = window.location.href;
+    let to: string = link.href;
+    let tos: Array<string> = [];
+    await chrome.storage.local.get(from).then((result) => {
+      if (result[from] !== undefined) {
+        console.log("before: ", result[from]);
+        tos = result[from];
+      }
+    });
+    if (tos.length > 0 && tos.indexOf(to) >= 0) {
+      console.log("no new visit");
+      return;
+    }
+    tos.push(to);
+    let fromTo: any = {};
+    fromTo[from] = tos;
+    await chrome.storage.local.set(fromTo);
+    chrome.storage.local.get(from, (result) => {
+      console.log("after: ", result);
+    });
+  });
+});
